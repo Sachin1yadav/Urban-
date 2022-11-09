35fa3114b9bae7516a0d8ad7f5ab3dd5b34d5d75
sachin_yadav <sy9084087@gmail.com>
2022-11-09 22:35:03 +0530

    day1

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+ 
+import './App.css';
+import Home from "./Homepage/Home"
+ 
+function App() {
+  return (
+    <div className="App">
+     <Home/>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Homepage/Home.jsx
@@ -0,0 +1,279 @@
+ 
+import "./Home.css"
+
+ function Home(){
+
+
+
+
+  return (
+    <div>
+      <div className="Navbar"> </div>
+    
+   
+  <div className="main"> 
+  <div>
+    <img className="docter" src="https://res.cloudinary.com/urbanclap/image/upload/q_40,f_auto/categories/category_v2/category_19be5040.jpeg" alt="" />
+    </div> 
+
+
+     </div>
+
+ {/* .....mid... */}
+  <div className="mid"> 
+      <div className="mid1">
+       <div>
+        <p style={{ fontFamily:"axiforma-regular, -apple-system, BlinkMacSystemFont",fontWeight:"bold",fontSize:"32px" ,lineHeight:"48px",textAlign:"start",letterSpacing:"normal"
+       ,color:"ffffff"}} >Why Urban Company?</p>
+       </div>
+       
+        <div className="mid1A">
+          <div><img style={{width:"70px" , height:"70px"}} src="https://res.cloudinary.com/urbanclap/image/upload/q_40,f_auto/categories/category_v2/category_29614d40.png" alt="" /> </div>
+          <div style={{marginLeft:"10px"}} > 
+            <p style={{  marginBottom:"-20px" ,fontSize:"20px",fontWeight:"bold" ,textAlign:"start"  }}  >Transparent pricing</p>
+            <p>See fixed prices before you book.No hidden charges</p>
+          </div>
+        </div>
+        <div className="mid1A">
+        <div><img style={{width:"70px" , height:"70px"}} src="https://res.cloudinary.com/urbanclap/image/upload/q_40,f_auto/categories/category_v2/category_2cc7d0d0.png" alt="" /> </div>
+        <div style={{marginLeft:"10px"}} > 
+            <p style={{  marginBottom:"-20px" ,fontSize:"20px",fontWeight:"bold" ,textAlign:"start"  }}  >Expert Only</p>
+            <p>Our professionals are well trained and have an-job expertise</p>
+          </div>
+        </div>
+        <div className="mid1A">
+        <div><img style={{width:"70px" , height:"70px"}} src="https://res.cloudinary.com/urbanclap/image/upload/q_40,f_auto/categories/category_v2/category_2caafa00.png" alt="" /> </div>
+        <div style={{marginLeft:"10px"}} > 
+            <p style={{  marginBottom:"-20px" ,fontSize:"20px",fontWeight:"bold" ,textAlign:"start"  }}  >Fully qeuipped</p>
+            <p>We bring everything needed to get the job done well</p>
+          </div>
+        </div>
+        
+
+      </div>
+      <div className="mid2">
+       <div className="mid2A">
+        <img src="./image/proImg1.PNG" alt="img"/> 
+        {/* <img src="https://res.cloudinary.com/urbanclap/image/upload/q_20,f_auto/categories/category_v2/category_cb4d9130.png" alt="" /> */}
+       </div>
+
+      </div>
+       
+  </div>
+{/* .....lineing..... */}
+ <div className="line" style={{width:"100%",height:"25px", backgroundColor:"rgb(240,240,240)"
+}}></div>
+{/* .....book professionals */}
+       
+     
+<div className="book">
+<div className="fill">
+  <div>
+    <p  style={{fontSize:"40px",fontWeight:"bold",lineHeight:"normal",textAlign:"start",letterSpacing:"normal"}}  >Book professionals from your phone</p>
+    <p style={{fontSize:"20px",textAlign:"start",marginTop:"-30px"}}>Enter your mobile number to get the App download link.</p>
+  </div>
+  <div className="allinput">
+    <select className="input1">
+      <option>IND</option>
+      <option>USA</option>
+      <option>CHAINA</option>
+      <option>RUSSIA</option>
+    </select >
+    <input className="input2" placeholder="Mobile Number" type="number"/>
+    <button className="input3">Send</button>
+  </div>
+  <div >
+     {/* <img className="download" src="https://www.freepnglogos.com/uploads/app-store-logo-png/google-play-and-apple-app-store-logos-22.png"/> */}
+ <img className="download" src="https://thewanderingkitchen.co.uk/wp-content/uploads/2019/01/playstore.png" />
+ <img className="download2" src="https://logos-download.com/wp-content/uploads/2016/06/Download_on_the_App_Store_logo.png" />
+  </div>
+</div>
+<div className="ph1">
+  <img className="ph1a" src="https://res.cloudinary.com/urbanclap/image/upload/w_300,f_auto/categories/category_v2/category_2d7045d0.png"/>
+</div>
+<div className="ph2">
+  <img className="ph2a" src="https://res.cloudinary.com/urbanclap/image/upload/w_350,f_auto/categories/category_v2/category_2d013050.png" alt="" />
+</div>
+</div>
+
+{/* ........footer.... */}
+
+<div className="footer">
+<div className="about" >
+  <p>About</p>
+  <p>UC Import</p>
+  <p>Terms&Condition</p>
+  <p>Privacy Policy</p>
+  <p>Intrest-Based Adverting</p>
+  <p>Anti Discimination</p>
+  <p>Partner Welfare Policy</p>
+  <p>Blog</p>
+  <p>Reviews</p>
+  <p>Near Me</p>
+  <p>Careers</p>
+  <p>Gift Cards</p>
+  <p>Contact Us</p>
+  <p>Quick Links</p>
+</div>
+<hr style={{marginTop:"30px",color:"gray",height:"0px"}} />
+<p style={{color:"white",fontWeight:"bold", fontFamily:"axiforma-regular, -apple-system, BlinkMacSystemFont",fontSize:"18px",marginLeft:"-1150px" }}>Serving</p>
+
+<div className="country"  style={{color:"white"}}>
+ <ul>
+  <li>
+    <span style={{marginLeft:"-1180px"}} >USA</span>
+    <ul className="city">
+      <li>Austin</li>
+      <li>Dollas</li>
+      <li>New York City</li>
+    </ul>
+  </li>
+  {/* .... */}
+  <li>
+    <span style={{marginLeft:"-1180px"}}  >IND</span>
+    <ul className="city">
+      <li>Agra</li>
+      <li>Ahmedabad</li>
+      <li>Alwar</li>
+      <li>Amritsar</li>
+      <li>Aurangabad</li>
+      <li>Banglore</li>
+      <li>Bhopal</li>
+      <li>Bhubanswer</li>
+      <li>Chandigarh Tricity</li>
+      <li>Chennai</li>
+      <li>Coimbatore</li>
+      <li>Cuttack</li>
+      <li>Dehradun</li>
+      <li>Delhi NCR</li>
+      <li>Guntur</li>
+      <li>Guwahti</li>
+      <li>Agra</li>
+      <li>Ahmedabad</li>
+      <li>Alwar</li>
+      <li>Amritsar</li>
+      <li>Aurangabad</li>
+      <li>Banglore</li>
+      <li>Bhopal</li>
+      <li>Bhubanswer</li>
+      <li>Chandigarh Tricity</li>
+      <li>Chennai</li>
+      <li>Coimbatore</li>
+      <li>Cuttack</li>
+      <li>Dehradun</li>
+      <li>Delhi NCR</li>
+      <li>Guntur</li>
+      <li>Guwahti</li>
+      <li>Agra</li>
+      <li>Ahmedabad</li>
+      <li>Alwar</li>
+      <li>Amritsar</li>
+      <li>Aurangabad</li>
+      <li>Banglore</li>
+      <li>Bhopal</li>
+      <li>Bhubanswer</li>
+      <li>Chandigarh Tricity</li>
+      <li>Chennai</li>
+      <li>Coimbatore</li>
+      <li>Cuttack</li>
+      <li>Dehradun</li>
+      <li>Delhi NCR</li>
+      <li>Guntur</li>
+      <li>Guwahti</li>
+      <li>Agra</li>
+      <li>Ahmedabad</li>
+      <li>Alwar</li>
+      <li>Amritsar</li>
+      <li>Aurangabad</li>
+      <li>Banglore</li>
+      <li>Bhopal</li>
+      <li>Bhubanswer</li>
+      <li>Chandigarh Tricity</li>
+      <li>Chennai</li>
+      <li>Coimbatore</li>
+      <li>Cuttack</li>
+      <li>Dehradun</li>
+      <li>Delhi NCR</li>
+      <li>Guntur</li>
+      <li>Guwahti</li>
+      <li>Agra</li>
+      <li>Ahmedabad</li>
+      <li>Alwar</li>
+      <li>Amritsar</li>
+      <li>Aurangabad</li>
+      <li>Banglore</li>
+      <li>Bhopal</li>
+      <li>Bhubanswer</li>
+      <li>Chandigarh Tricity</li>
+      <li>Chennai</li>
+      <li>Coimbatore</li>
+      <li>Cuttack</li>
+      <li>Dehradun</li>
+      <li>Delhi NCR</li>
+      <li>Guntur</li>
+      <li>Guwahti</li>
+    </ul>
+  </li>
+  {/* ... */}
+  <li>
+    <span  style={{marginLeft:"-1180px"}} >USE</span>
+    <ul className="city">
+      <li>Abu Dhabi</li>
+      <li>Dubai</li>
+      <li>Sharjah</li>
+    </ul>
+  </li>
+  {/* .... */}
+  <li>
+    <span  style={{marginLeft:"-1180px"}} >AUS</span>
+    <ul className="city">
+      <li>Melbourm</li>
+      <li>Sydney</li>
+    </ul>
+  </li>
+  {/* .... */}
+  <li>
+    <span style={{marginLeft:"-1180px"}}  >SGP</span>
+    <ul className="city">
+      <li>Singapore</li>
+    </ul>
+  </li>
+  {/* .... */}
+  <li>
+    <span style={{marginLeft:"-1180px" }}  >KSA</span>
+    <ul className="city">
+      <li>Jeddah</li>
+      <li>Riyadh</li>
+    </ul>
+  </li>
+ </ul>
+
+
+
+
+</div>
+ 
+
+
+
+
+
+
+
+</div>
+
+{/* ...End */}
+  </div>
+  )
+ }
+
+ export default Home
+
+
+
+
+
+
+
+
+ 
